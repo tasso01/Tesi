b8889b1e2ddfa4e716c4584ea084237c82fda3f7
unknown <francescotasso16@gmail.com>
2025-02-19 01:59:15 +0100

    download CIF from PDB_set

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-def estrai_pdb_id(file_path):
+import os
+import requests
+
+def from_file_to_set(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         contenuto = file.read()
-        id_app = contenuto.split(',')
-        pdb_id = {id.strip() for id in id_app}
-    return pdb_id
+        pdb_ids_app = contenuto.split(',')
+        pdb_ids = {id.strip() for id in pdb_ids_app}
+    return pdb_ids
 
-print(estrai_pdb_id('example_PDB_ID.txt'))
+def download_cif_from_set(pdb_ids, destination_folder="cif_files"):
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+    base_url = "https://files.rcsb.org/download/"
+    for pdb_id in pdb_ids:
+        url = f"{base_url}{pdb_id}.cif"
+        file_path = os.path.join(destination_folder, f"{pdb_id}.cif")
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            with open(file_path, 'wb') as file:
+                file.write(response.content)
+            print(f"Scaricato: {pdb_id}.cif")
+        except requests.exceptions.RequestException as e:
+            print(f"Errore nel download di {pdb_id}: {e}")
+
+prova = from_file_to_set('example_PDB_ID.txt')
+download_cif_from_set(prova)
