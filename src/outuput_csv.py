3ec547eabd5e520de6465f2a1a6e7db1acb4c06e
unknown <francescotasso16@gmail.com>
2025-03-28 20:20:12 +0100

    insert output polymer/molecule

--- src/outuput_csv.py
+++ src/outuput_csv.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from Bio.PDB.MMCIF2Dict import MMCIF2Dict
 from src import get_tool, get_molecule_type, get_polymer_type
 
 PDB_IDS = []
@@ -41,84 +42,79 @@ def check_tool():
     tool = get_tool()
     match tool:
         case "FR3D":
-            fr3d_output()
+            output_generator("fr3d")
         case "baRNAba":
-            barnaba_output()
+            output_generator("barnaba")
         case "RNAView":
-            rnaview_output()
+            output_generator("rnaview")
     save_csv()
 
-def fr3d_output():
-    bpseq_folder = "output\\fr3d_bpseq"
-    txt_folder = "output\\fr3d_txt"
+def output_generator(tool):
+    bpseq_folder = f"output\\{tool}_bpseq"
+    txt_folder = f"output\\{tool}_txt"
     for filename in os.listdir(bpseq_folder):
         bpseq_path = os.path.join(bpseq_folder, filename)
         pdb_id = os.path.splitext(filename)[0]
+        pdb = pdb_id.split('_')[0]
+        chain = pdb_id.split('_')[1].split('-')[0]
         txt_path = ""
         for file in os.listdir(txt_folder):
             pdb_id2 = os.path.splitext(file)[0]
             if (pdb_id == pdb_id2):
                 txt_path = os.path.join(txt_folder, file)
-        pdb = pdb_id.split('_')[0]
-        chain = pdb_id.split('_')[1].split('-')[0]
         add_pdb_id(pdb)
         add_chain(chain)
-        add_bpseq_output(bpseq_path)
-        add_txt_output(txt_path)
-    output_number = len(os.listdir(bpseq_folder))
-    insert_from_init(output_number)
-
-def barnaba_output():
-    bpseq_folder = "output\\barnaba_bpseq"
-    txt_folder = "output\\barnaba_txt"
-    for filename in os.listdir(bpseq_folder):
-        bpseq_path = os.path.join(bpseq_folder, filename)
-        pdb_id = os.path.splitext(filename)[0]
-        txt_path = ""
-        for file in os.listdir(txt_folder):
-            pdb_id2 = os.path.splitext(file)[0]
-            if (pdb_id == pdb_id2):
-                txt_path = os.path.join(txt_folder, file)
-        pdb = pdb_id.split('_')[0]
-        chain = pdb_id.split('_')[1].split('-')[0]
-        add_pdb_id(pdb)
-        add_chain(chain)
-        add_bpseq_output(bpseq_path)
-        add_txt_output(txt_path)
-    output_number = len(os.listdir(bpseq_folder))
-    insert_from_init(output_number)
-
-def rnaview_output():
-    bpseq_folder = "output\\rnaview_bpseq"
-    txt_folder = "output\\rnaview_txt"
-    for filename in os.listdir(bpseq_folder):
-        bpseq_path = os.path.join(bpseq_folder, filename)
-        pdb_id = os.path.splitext(filename)[0]
-        pdb = pdb_id.split('_')[0]
-        txt_path = ""
-        for file in os.listdir(txt_folder):
-            pdb_id2 = os.path.splitext(file)[0]
-            if (pdb_id == pdb_id2):
-                txt_path = os.path.join(txt_folder, file)
-        chain = pdb_id.split('_')[1].split('-')[0]
-        add_pdb_id(pdb)
-        add_chain(chain)
+        if (get_polymer_type()):
+            add_molecule(molecule_from_polymer(pdb, chain))
         add_bpseq_output(bpseq_path)
         add_txt_output(txt_path)
     output_number = len(os.listdir(bpseq_folder))
     insert_from_init(output_number)
 
 def insert_from_init(n):
+    polymer = polymer_from_molecule()
     for _ in range(n):
         add_tool(get_tool())
         if get_molecule_type():
             add_molecule(get_molecule_type())
-            add_polymer("")
+            add_polymer(polymer)
         elif get_polymer_type():
             add_polymer(get_polymer_type())
-            add_molecule("")
         else:
             raise TypeError("Polimero o Molecola mancante")
+
+def from_id_to_file(pdb_id):
+    for file in os.listdir("files_cif"):
+        if pdb_id == os.path.splitext(file)[0]:
+            return os.path.join("files_cif", file)
+
+def molecule_from_polymer(pdb_id, chain):
+    mmcif_file = from_id_to_file(pdb_id)
+    mmcif_dict = MMCIF2Dict(mmcif_file)
+    entity_ids = mmcif_dict.get("_entity.id", [])
+    descriptions = mmcif_dict.get("_entity.pdbx_description", [])
+    if isinstance(entity_ids, str):
+        entity_ids = [entity_ids]
+    if isinstance(descriptions, str):
+        descriptions = [descriptions]
+    entity_map = dict(zip(entity_ids, descriptions))
+    return entity_map.get(str(chain), None)
+
+def polymer_from_molecule():
+    first_mmcif_id = os.listdir("files_cif_id")[0]
+    pdb_id = os.path.splitext(first_mmcif_id)[0]
+    pdb = pdb_id.split('_')[0]
+    chain = pdb_id.split('_')[1]
+    mmcif_file = os.path.join("files_cif", f"{pdb}.cif")
+    mmcif_dict = MMCIF2Dict(mmcif_file)
+    entity_ids = mmcif_dict.get("_entity_poly.entity_id", [])
+    entity_types = mmcif_dict.get("_entity_poly.type", [])
+    if isinstance(entity_ids, str):
+        entity_ids = [entity_ids]
+    if isinstance(entity_types, str):
+        entity_types = [entity_types]
+    entity_map = dict(zip(entity_ids, entity_types))
+    return entity_map.get(str(chain), None)
 
 def save_csv():
     nome_file = "output.csv"
